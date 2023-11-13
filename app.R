dfc92c3fccdb9d95bcd9749654ec902ac0bfc9b3
Thomas Deckers <thomasdeck14@gmail.com>
2023-11-13 13:27:23 -0800

    contructed base BCL app by following Dean Attali's tutorial (https://deanattali.com/blog/building-shiny-apps-tutorial/)

---
+++ app.R
@@ -0,0 +1,59 @@
+library(shiny)
+library(ggplot2)
+library(dplyr)
+
+bcl <- read.csv("bcl-data.csv", stringsAsFactors = FALSE)
+
+ui <- fluidPage(
+  titlePanel("BC Liquor Store prices"),
+  sidebarLayout(
+    sidebarPanel(sliderInput("priceInput", "Price", min = 0, max = 100,
+                             value = c(25, 40), pre = "$"),
+                 radioButtons("typeInput", "Product type",
+                              choices = c("BEER", "REFRESHMENT", "SPIRITS", "WINE"),
+                              selected = "WINE"),
+                 uiOutput("countryOutput")),
+    mainPanel(plotOutput("coolplot"),
+              br(),br(),
+              tableOutput("results"))
+  )
+  )
+server <- function(input, output) {
+  filtered <- reactive({
+
+    if (is.null(input$countryInput)) {
+      return(NULL)
+    }
+
+    bcl %>%
+      filter(Price >= input$priceInput[1],
+             Price <= input$priceInput[2],
+             Type == input$typeInput,
+             Country == input$countryInput
+      )
+  })
+
+
+  output$coolplot <- renderPlot({
+    if (is.null(filtered())) {
+      return()
+    }
+
+
+    ggplot(filtered(), aes(Alcohol_Content)) +
+      geom_histogram()
+})
+
+output$results <- renderTable({
+  filtered()
+})
+
+output$countryOutput <- renderUI({
+  selectInput("countryInput", "Country",
+              sort(unique(bcl$Country)),
+              selected = "CANADA")
+})
+
+}
+shinyApp(ui = ui, server = server)
+
