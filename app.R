4546c24c66aaaa8b2c9965a09f88427f9a52575e
Thomas Deckers <thomasdeck14@gmail.com>
2023-11-13 16:15:21 -0800

    Reformatted code, added organizational comments

--- app.R
+++ app.R
@@ -5,93 +5,123 @@ library(DT)
 
 bcl <- read.csv("bcl-data.csv", stringsAsFactors = FALSE)
 
-ui <- fluidPage(
-  titlePanel("BC Liquor Store Prices"),
-  sidebarLayout(
-    sidebarPanel(sliderInput("priceInput", "Price", min = 0, max = 100,
-                             value = c(25, 40), pre = "$"),
-                 radioButtons("typeInput", "Product type",
-                              choices = c("BEER", "REFRESHMENT", "SPIRITS", "WINE"),
-                              selected = "WINE"),
-                 uiOutput("countryOutput"),
+ui <- fluidPage(titlePanel("BC Liquor Store Prices"),
+                sidebarLayout(
+
+                  ## Sidebar ##
+                  sidebarPanel(
+
+                    sliderInput(
+                      "priceInput",
+                      "Price",
+                      min = 0,
+                      max = 100,
+                      value = c(25, 40),
+                      pre = "$"
+                    ),
+
+                    radioButtons(
+                      "typeInput",
+                      "Product type",
+                      choices = c("BEER", "REFRESHMENT", "SPIRITS", "WINE"),
+                      selected = "WINE"
+                    ),
+
+                    uiOutput("countryOutput"),
 
 
-                 # Feature 1: Download Button.
-                 # This will let users save the results of their specific filters
-                 # and access them offline
-                 strong("Download table"),
-                 downloadButton("downloadData", "Download")),
-    mainPanel(
-                # Feature 2: Separate main display into tabs.
-                # This allows easy toggling between the plot and data table,
-                # without the need for scrolling.
-                tabsetPanel(
-                  tabPanel("Alcohol Percent Plot", plotOutput("coolplot")),
+                    # Feature 1: Download Button.
+                    # This will let users save the results of their specific filters
+                    # and access them offline
+                    strong("Download table"),
+                    downloadButton("downloadData", "Download")
+                  ),
 
-                  # Feature 3: Interactive table with DT.
-                  # Easily allows users to sort results, search for entries,
-                  # and page through results, creating a much more flexible
-                  # viewing experience
-                  tabPanel("Matched Results", DT::dataTableOutput("results"))
-                            ),
-    )
+                  ## Main Panel ##
+                  mainPanel(
 
-  )
-  )
+                    # Feature 2: Separate main display into tabs.
+                    # This allows easy toggling between the plot and data table,
+                    # without the need for scrolling.
+                    tabsetPanel(
+                      tabPanel("Alcohol Percent Plot", plotOutput("alcoholPctPlot")),
+
+                      # Feature 3: Interactive table with DT.
+                      # Easily allows users to sort results, search for entries,
+                      # and page through results, creating a much more flexible
+                      # viewing experience
+                      tabPanel("Matched Results", DT::dataTableOutput("results"))
+                    ),)
+
+                ))
+
+
 server <- function(input, output) {
-  filtered <- reactive({
 
+  ## Setup ##
+
+  # Create data table
+  filtered <- reactive({
     if (is.null(input$countryInput)) {
       return(NULL)
     }
 
+
     bcl %>%
-      filter(Price >= input$priceInput[1],
-             Price <= input$priceInput[2],
-             Type == input$typeInput,
-             Country == input$countryInput
+      filter(
+        Price >= input$priceInput[1],
+        Price <= input$priceInput[2],
+        Type == input$typeInput,
+        Country == input$countryInput
       )
   })
 
+  ## Main Panel ##
 
-  output$coolplot <- renderPlot({
+  # Plot
+  output$alcoholPctPlot <- renderPlot({
     if (is.null(filtered())) {
       return()
     }
 
-
     ggplot(filtered(), aes(Alcohol_Content)) +
-      geom_histogram(bins = 30)
-})
+      geom_histogram(bins = 30) +
+      labs(x = "Alcohol content", y = "Count") +
+      theme_minimal()
+  })
 
-# Server code for feature 3
-# Add custom column names, to remove the underscote from "Alchol Content"
-output$results <- DT::renderDataTable({
-  filtered()
-}, colnames = c("Type", "Subtype", "Country", "Name", "Alcohol Content", "Price", "Sweetness")
-)
+  # Table
+  # Server code for feature 3
+  # Add custom column names, to remove the underscore from "Alcohol Content"
+  output$results <- DT::renderDataTable({
+    filtered()
+  }, colnames = c(
+    "Type",
+    "Subtype",
+    "Country",
+    "Name",
+    "Alcohol Content",
+    "Price",
+    "Sweetness"
+  ))
 
-output$countryOutput <- renderUI({
-  selectInput("countryInput", "Country",
-              sort(unique(bcl$Country)),
-              selected = "CANADA")
-})
+  ## Sidebar ##
 
+  # Country selector
+  output$countryOutput <- renderUI({
+    selectInput("countryInput", "Country",
+                sort(unique(bcl$Country)),
+                selected = "CANADA")
+  })
 
-# Server code for feature 1
-output$downloadData <- downloadHandler(
-  filename = "BCLiquor_search_results.csv",
-  content = function(file) {
-    write.csv(filtered(), file)
-  }
-)
-
-
-
-
-
-
+  # Download button
+  # Server code for feature 1
+  output$downloadData <- downloadHandler(
+    filename = "BCLiquor_search_results.csv",
+    content = function(file) {
+      write.csv(filtered(), file)
+    }
+  )
 
 }
 shinyApp(ui = ui, server = server)
-
